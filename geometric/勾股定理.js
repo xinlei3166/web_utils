6c585b4b1ba23fb6b05a864f7a6fb5246ff60c2b
君惜 <xinlei3166@126.com>
2021-02-04 19:12:42 +0800

    feat: add webstorm.alias.js.

--- geometric/勾股定理.js
+++ geometric/勾股定理.js
@@ -3,3 +3,4 @@ const a = 10  // 勾
 const b = 20  // 股
 const c = Math.pow(a, 2) + Math.pow(b, 2) // 弦
 console.log(c)
+console.log(Math.sqrt(c))
---
+++ utils/webstorm.alias.js
@@ -0,0 +1,11 @@
+const path = require('path')
+
+module.exports = {
+  resolve: {
+    extensions: ['.js', '.json', '.vue', '.ts'],
+    root: path.resolve(__dirname),
+    alias: {
+      '@': path.resolve(__dirname)
+    },
+  },
+}
